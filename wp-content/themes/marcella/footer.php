75366d1b565217cf01c6bb86a7465bcd59860336
marcella-concas <marcellinabumbum@gmail.com>
2017-06-06 10:31:06 +0200

    Add files via upload

---
+++ wp-content/themes/marcella/footer.php
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="vendors/js/jquery.min.js" ></script>
+<script type="text/javascript" src="vendors/js/bootstrap.min.js" ></script>
+<script type="text/javascript" src="js/fixtures.js"></script>
+</body>
+</html>
---
+++ wp-content/themes/marcella/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="vendors/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="jumbotron">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mymenu" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#"></a>
+					</div>
+
+
+					<div class="collapse navbar-collapse" id="mymenu">
+						<ul class="nav navbar-nav navbar-right">
+							<li class="active"><a href="#">Natura in festa <span class="sr-only">(current)</span></a></li>
+							<li><a href="#">luoghi</a></li>
+							<li><a href="#">saperi</a></li>
+							<li><a href="#">attrazioni</a></li>
+
+						</ul>
+
+
+
+					</div>
+				</div>
+			</nav>
