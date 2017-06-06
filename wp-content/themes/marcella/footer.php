950b9ba22de19887e3c1c2206789bb53890890f0
marcella-concas <marcellinabumbum@gmail.com>
2017-06-06 22:09:41 +0200

    Add files via upload

--- wp-content/themes/marcella/footer.php
+++ wp-content/themes/marcella/footer.php
@@ -1,5 +1,3 @@
-<script type="text/javascript" src="vendors/js/jquery.min.js" ></script>
-<script type="text/javascript" src="vendors/js/bootstrap.min.js" ></script>
-<script type="text/javascript" src="js/fixtures.js"></script>
+<?php wp_footer();?>
 </body>
 </html>
--- wp-content/themes/marcella/index.php
+++ wp-content/themes/marcella/index.php
@@ -1,42 +1,43 @@
  <?php get_header(); ?>
  <div class="row">
-<div class="col-md-12 Benvenuti">
-	<h1>Siete invitati a Natura in festa</h1>
-	<h4>Qui troverete tutti i dettagli riguardanti la manifestazione </h4>
-</div>
-</div>
+ 	<div class="col-md-12 Benvenuti">
+ 		<h1>Siete invitati a Natura in festa</h1>
+ 		<h4>Qui troverete tutti i dettagli riguardanti la manifestazione </h4>
+ 	</div>
+ </div>
 
 
-<!--form begin-->
-<div class="container-fluid">
-	<div class="row text-center">
-		<h1>Contattaci</h1>
-		<h3>Scrivi i tuoi dati negli appositi spazi</h3>
-	</div>
+ <!--form begin-->
+ <div class="container-fluid">
+ 	<div class="row text-center">
+ 		<h1>Contattaci</h1>
+ 		<h3>Scrivi i tuoi dati negli appositi spazi</h3>
+ 	</div>
+ </div>
 
-	<form class="concas-form">
-		<div class="row">
+ <form class="concas-form">
+ 	<div class="row">
 
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="exampleInputEmail1">Email</label>
-					<input type="email" class="form-control" id="exampleInputEmail1" placeholder=" Digita qui la tua Email" required autofocus="">
-				</div>
-				<div class="form-group">
-					<label for="exampleInputPassword1"> Password</label>
-					<input type="password" class="form-control" id="exampleInputPassword1" placeholder=" Digita qui la tua Password" required>
-				</div>
-			</div>
+ 		<div class="col-md-6">
+ 			<div class="form-group">
+ 				<label for="exampleInputEmail1">Email</label>
+ 				<input type="email" class="form-control" id="exampleInputEmail1" placeholder=" Digita qui la tua Email" required autofocus="">
+ 			</div>
+ 			<div class="form-group">
+ 				<label for="exampleInputPassword1"> Password</label>
+ 				<input type="password" class="form-control" id="exampleInputPassword1" placeholder=" Digita qui la tua Password" required>
+ 			</div>
+ 		</div>
 
-			<div class="col-md-6">
-				<label for="exampleInputEmail1">Scrivi un commento</label>
-				<textarea class="form-control" rows="3" required></textarea>
-			</div>
+ 		<div class="col-md-6">
+ 			<label for="exampleInputEmail1">Scrivi un commento</label>
+ 			<textarea class="form-control" rows="3" required></textarea>
+ 		</div>
 
-		</div>
-		<div class="row text-center">
-			<button type="submit" class="btn btn-default">Submit</button>
-		</form>
-	</div>
+ 	</div>
+ 	<div class="row text-center">
+ 		<button type="submit" class="btn btn-default">Submit</button>
+ 	</form>
+ </div>
 </div> 
 <?php get_footer();  ?>
