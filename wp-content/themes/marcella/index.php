c1fd855ed553f5316cb9af1059477e051c3e1c6c
marcella-concas <marcellinabumbum@gmail.com>
2017-06-07 17:44:56 +0200

    Add files via upload

--- wp-content/themes/marcella/index.php
+++ wp-content/themes/marcella/index.php
@@ -8,35 +8,14 @@
 
 
  <!--form begin-->
- <div class="container-fluid">
- 	<div class="row text-center">
- 		<h1>Contattaci</h1>
- 		<h3>Scrivi i tuoi dati negli appositi spazi</h3>
- 	</div>
- </div>
+ 
+ 	<div class="row text-center" id="mymenu">
 
- <form class="concas-form">
- 	<div class="row">
 
- 		<div class="col-md-6">
- 			<div class="form-group">
- 				<label for="exampleInputEmail1">Email</label>
- 				<input type="email" class="form-control" id="exampleInputEmail1" placeholder=" Digita qui la tua Email" required autofocus="">
- 			</div>
- 			<div class="form-group">
- 				<label for="exampleInputPassword1"> Password</label>
- 				<input type="password" class="form-control" id="exampleInputPassword1" placeholder=" Digita qui la tua Password" required>
- 			</div>
- 		</div>
-
- 		<div class="col-md-6">
- 			<label for="exampleInputEmail1">Scrivi un commento</label>
- 			<textarea class="form-control" rows="3" required></textarea>
- 		</div>
-
- 	</div>
- 	<div class="row text-center">
- 		<button type="submit" class="btn btn-default">Submit</button>
+  </div>
+  <div class="row form-row">
+    <form>
+      <?php echo do_shortcode('[contact-form-7 id="4" title="Contact form 1"]'); ?>
  	</form>
  </div>
 </div> 
