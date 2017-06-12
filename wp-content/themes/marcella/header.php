b5c725c692cbc613d77042cdccc62b99dc19acc3
marcella-concas <marcellinabumbum@gmail.com>
2017-06-12 17:15:56 +0200

    Add files via upload

--- wp-content/themes/marcella/header.php
+++ wp-content/themes/marcella/header.php
@@ -12,7 +12,7 @@
 				<div class="container-fluid">
 
 					<div class="navbar-header">
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mymenu" aria-expanded="false">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 							<span class="sr-only">Toggle navigation</span>
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
@@ -22,7 +22,7 @@
 					</div>
 
 
-					<div class="collapse navbar-collapse" id="mymenu">
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav navbar-right">
 							<li class="active"><a href="#">Natura in festa <span class="sr-only">(current)</span></a></li>
 							<li><a href="#">luoghi</a></li>
